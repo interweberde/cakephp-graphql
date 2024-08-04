47de7be97a5172895308c5b4c49e727d0fcd23fe
Daniel Melchior <daniel.melchior@interweber.de>
2024-08-04 21:27:20 +0200

    chore: migrate Sorter to SelectQuery and Direction enum

---
+++ src/Sorter/Direction.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Interweber\GraphQL\Sorter;
+
+enum Direction: string {
+	case ASC = 'asc';
+	case DESC = 'desc';
+}
--- src/Sorter/Sorter.php
+++ src/Sorter/Sorter.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace Interweber\GraphQL\Sorter;
 
 use Cake\Database\Expression\OrderClauseExpression;
-use Cake\ORM\Query;
+use Cake\ORM\Query\SelectQuery;
 
 class Sorter {
 	/**
@@ -13,7 +13,19 @@ class Sorter {
 	public function __construct(protected array $orderExpressions = []) {
 	}
 
-	public function apply(Query $q): Query {
-		return $q->order($this->orderExpressions);
+	public function apply(SelectQuery $q): SelectQuery {
+		foreach ($this->orderExpressions as $expr) {
+			if (!$expr instanceof OrderClauseExpression) {
+				continue;
+			}
+
+			$expr->setField($q->getRepository()->aliasField($expr->getField()));
+		}
+
+		return $q->orderBy($this->orderExpressions);
+	}
+
+	public static function createOrderExpression(string $field, Direction $direction): OrderClauseExpression {
+		return new OrderClauseExpression($field, $direction->value);
 	}
 }
