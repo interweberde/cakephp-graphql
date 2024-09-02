d15f0f3c3576febad560b8b075d8f9484f2fda50
Daniel Melchior <daniel.melchior@interweber.de>
2024-09-02 11:53:56 +0200

    fix: correctly handle callables in sorter

--- src/Sorter/Sorter.php
+++ src/Sorter/Sorter.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 namespace Interweber\GraphQL\Sorter;
 
 use Cake\Database\Expression\OrderClauseExpression;
+use Cake\Database\Expression\QueryExpression;
 use Cake\ORM\Query\SelectQuery;
 
 class Sorter {
 	/**
-	 * @param array<OrderClauseExpression|\Closure> $orderExpressions
+	 * @param array<OrderClauseExpression|callable(QueryExpression, SelectQuery): OrderClauseExpression> $orderExpressions
 	 */
 	public function __construct(protected array $orderExpressions = []) {
 	}
 
 	public function apply(SelectQuery $q): SelectQuery {
-		foreach ($this->orderExpressions as $expr) {
+		foreach ($this->orderExpressions as $key => $expr) {
 			if (!$expr instanceof OrderClauseExpression) {
+				$this->orderExpressions[$key] = $expr($q->newExpr(), $q);
 				continue;
 			}
 
