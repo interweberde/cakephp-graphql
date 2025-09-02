85d4c04256235ad51769c2cde576e6df4809b4ec
Daniel Melchior <daniel.melchior@interweber.de>
2025-09-02 16:17:33 +0200

    fix: correctly map contain values from dependencies

--- src/Classes/QueryOptimizer.php
+++ src/Classes/QueryOptimizer.php
@@ -330,7 +330,7 @@ class QueryOptimizer {
 			}
 
 			// we get an array of booleans with numeric keys when the same field is requested multiple times
-			if (is_array($value) && !array_filter($value, fn ($key) => !is_numeric($key), ARRAY_FILTER_USE_KEY)) {
+			if (is_array($value) && !array_filter($value, fn ($value, $key) => !is_bool($value) || !is_numeric($key), ARRAY_FILTER_USE_BOTH)) {
 				$value = (bool) in_array(true, $value);
 			}
 
